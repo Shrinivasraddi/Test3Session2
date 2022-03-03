b83b216b14f693b574af9a2c1ad9fd2d0e6a5934
Shrinivasraddi <97089356+Shrinivasraddi@users.noreply.github.com>
2022-03-03 08:53:47 +0000

    p1

--- p1final.c
+++ p1final.c
@@ -1,1 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
+/*int struct_fraction;
+{
+  int num,den;
+};
+typedef struct_fraction Fraction;*/
+int gcd(int a,int b)
+{
+  int t;
+    while(b != 0)
+  {
+     t = b;
+     b = a%b;
+     a = t;
+  }
+    return a;
+}
+int main()
+{
+  printf("%d\n",gcd(16,24));
+  return 0;
+}
